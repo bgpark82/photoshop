f17eb1ca3038714992dd0e493d77ee5d02a0460f
bgpark <bgpark@citus.co.kr>
2021-03-31 17:14:14 +0900

    feat: web 세팅

---
+++ web/pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Index = () => {
+    return (
+        <div>
+            hello
+        </div>
+    );
+};
+
+Index.propTypes = {
+
+};
+
+export default Index;
