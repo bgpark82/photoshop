adf07ef47246b17086cfa9c549f362cd1ec26f1c
bgpark <bgpark@citus.co.kr>
2021-04-02 11:23:09 +0900

    feat: 파일 업로드 서비스 구현

--- api/src/main/java/com/bgpark/photoshop/controller/FileUploadController.java
+++ api/src/main/java/com/bgpark/photoshop/controller/FileUploadController.java
@@ -1,5 +1,7 @@
 package com.bgpark.photoshop.controller;
 
+import com.bgpark.photoshop.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,15 +9,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.net.URI;
 
 @RestController
 @RequestMapping("/api/v1/upload")
+@RequiredArgsConstructor
 public class FileUploadController {
 
+    private final FileUploadService fileUploadService;
+
     @PostMapping
-    public ResponseEntity upload(@RequestParam MultipartFile file) {
-        System.out.println(file);
+    public ResponseEntity upload(@RequestParam("file") MultipartFile file) throws IOException, InterruptedException {
+        fileUploadService.uploadS3(file);
         return ResponseEntity.created(URI.create("/")).build();
     }
 }
--- api/src/main/java/com/bgpark/photoshop/service/FileUploadService.java
+++ api/src/main/java/com/bgpark/photoshop/service/FileUploadService.java
@@ -35,5 +35,4 @@ public class FileUploadService {
                 .mediaType(photo)
                 .build();
     }
-
 }
