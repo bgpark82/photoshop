a13080b88f121cfe95572f5988e8e959f85098ae
bgpark <bgpark@citus.co.kr>
2021-05-03 15:25:49 +0900

    test: session 로그인 interceptor 구현

---
+++ api/src/main/java/com/bgpark/photoshop/domain/auth/AuthConfig.java
@@ -0,0 +1,14 @@
+package com.bgpark.photoshop.domain.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class AuthConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionAuthenticationInterceptor()).addPathPatterns("/api/v1/login");
+    }
+}
---
+++ api/src/main/java/com/bgpark/photoshop/domain/auth/SessionAuthenticationInterceptor.java
@@ -0,0 +1,19 @@
+package com.bgpark.photoshop.domain.auth;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//https://soon-devblog.tistory.com/2
+public class SessionAuthenticationInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String email = request.getParameter("email");
+
+        request.getSession().setAttribute("email", email);
+        response.setStatus(HttpServletResponse.SC_OK);
+        return false;
+    }
+}
