383cc759749232880f78f33f6caa1fe38c46728e
bgpark <bgpark@citus.co.kr>
2021-05-07 11:33:10 +0900

    fix: @ExceptionHandler 추가

--- api/src/main/java/com/bgpark/photoshop/exception/GlobalErrorController.java
+++ api/src/main/java/com/bgpark/photoshop/exception/GlobalErrorController.java
@@ -2,6 +2,7 @@ package com.bgpark.photoshop.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,11 +10,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalErrorController {
 
     @ResponseBody
+    @ExceptionHandler(EmptyMultipartException.class)
     public ResponseEntity emptyMultipartException(EmptyMultipartException e) {
         return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
 
     @ResponseBody
+    @ExceptionHandler(UserNotFoundException.class)
     public ResponseEntity userNotFoundException(UserNotFoundException e) {
         return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
     }
